e1eae4ebadc49d27df304374da4b96e6581304d6
Chaya <mamatha.anjaneyamurthy@gmail.com>
2019-10-19 21:15:11 +0530

    change made

--- AppletHelloWorld/MyFirstApplet.java
+++ AppletHelloWorld/MyFirstApplet.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 
 public class MyFirstApplet extends Applet {
 
-    private static final long serialVersionUID = -3783584337636720597L;
+   // private static final long serialVersionUID = -3783584337636720597L;
 
     /**
      * The init method will be called when the applet is launched
@@ -21,5 +21,14 @@ public class MyFirstApplet extends Applet {
     public void draw(Graphics g) {
         g.drawString("Greetings", 10, 100);
     }
+    public void paint(Graphics g)
+   {
+    System.out.println("Hello there");
+    g.drawString("Drawing an oval in our applet", 0,10);
+    g.drawOval(50,50,100,100);		//draws an Oval in applet window
+    g.drawRect(150,150,100,100);		//draws a Rectangle in applet window
+    g.drawLine(270,270,350,350);		//draws a Line in applet window
+   }
+}
 
 }
